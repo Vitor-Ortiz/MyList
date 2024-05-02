24ec75cc63a657ca6c47e7dbcc10ef215542b867
anglusa <otavio.baroni@etec.sp.gov.br>
2024-05-02 11:38:41 -0300

    finalizaçao da tela inicial

--- Mylist/src/app/index.js
+++ Mylist/src/app/index.js
@@ -1,22 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import {Link} from 'expo-router';
+import { Link } from 'expo-router';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Welcome() {
   return (
     <View style={styles.container}>
       <View style={styles.containerImage}>
-        <Image source={require('../assets/shitpost.jpg')} style={styles.logo}/>
-
+        <Image source={require('../assets/shitpost.jpg')}
+          style={styles.logo} />
       </View>
-      <View>
-
+      <View style={styles.Content}>
+        <Text style={styles.title}>MyList</Text>
+        <Text styçe={styles.text}> monte sua lista e não esqueça mais o que precisa ao sair!</Text>
+        <Link style={styles.button} href={"/home"}>
+          <Text style={styles.buttonText}> Acessar</Text>
+        </Link>
       </View>
-      <Text>Welcome!</Text>
-      <Link href={"/home"}>
-      <Text> Acessar</Text>
-      </Link>
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor='#000' />
     </View>
   );
 }
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
- 
+
   },
   containerImage: {
     flex: 2,
@@ -38,4 +38,38 @@ const styles = StyleSheet.create({
     borderRadius: 115,
     resizeMode: 'cover',
   },
+Content: {
+  flex: 1,
+  backgroundColor: '#dadada',
+  borderTopLeftRadius: 25,
+  borderTopRightRadius: 25,
+  paddingHorizontal: '5%',
+  
+},
+title:{
+  fontSize: 30,
+  fontWeight: 'bold',
+  marginTop: 28,
+  marginBottom: 20,
+  textAlign: 'center'
+},
+text: {
+  fontSize: 16,
+  color: 'gray',
+},
+button: {
+  position: 'absolute',
+  backgroundColor: '#000',
+  bottom: '15%',
+  alignSelf: 'center',
+  borderRadius: 50,
+  paddingVertical: 15,
+  width: '60%',
+  textAlign: 'center',
+},
+buttonText:{
+  fontSize: 20,
+  color: '#fff',
+  fontWeight: 'bold',
+}
 });
